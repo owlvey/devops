6df851d42e69daeaea27013c3aca3c9e56a8b430
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2020-07-03 20:10:49 -0500

    add

--- devops-local-setup.bat
+++ devops-local-setup.bat
@@ -35,3 +35,28 @@ TIMEOUT 5
 rmdir /Q /S cluster 
 TIMEOUT 5
 mkdir cluster 
+
+
+pushd cluster 
+git clone --branch develop git@github.com:owlvey/owlvey_falcon.git
+rmdir /Q /S "owlvey_falcon/.git"
+git clone --branch develop git@github.com:owlvey/owlvey_identity.git
+rmdir /Q /S "owlvey_identity/.git"
+git clone --branch develop git@github.com:owlvey/owlvey_falcon_ui.git
+rmdir /Q /S "owlvey_falcon_ui/.git"
+
+popd
+
+python ./local-builder.py
+
+pushd cluster 
+
+rem  build.bat
+
+popd
+
+pushd cluster 
+
+REM cluster.bat
+
+popd
